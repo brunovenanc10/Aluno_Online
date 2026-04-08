1620519128f8ce0de2b7de514726fbc849f11fd9
Bruno Venancio <brunin207.s@gmail.com>
2026-04-07 21:43:08 -0300

    Delete Aluno_Onlinee directory

--- Aluno_Onlinee/src/main/java/br/com/alunoonline/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.alunoonline.repository;
-
-import br.com.alunoonline.model.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-}
--- Aluno_Onlinee/src/main/java/br/com/alunoonline/repository/ProfessorRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.alunoonline.repository;
-
-import br.com.alunoonline.model.Professor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-}
